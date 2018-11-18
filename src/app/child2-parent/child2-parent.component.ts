64fff5c3268dc262dba22120f3ee6e14629a0919
gubali <masif_apps@live.com>
2018-11-18 09:05:04 +0530

    done

--- src/app/child2-parent/child2-parent.component.ts
+++ src/app/child2-parent/child2-parent.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
   styleUrls: ['./child2-parent.component.css'],
 })
 export class Child2ParentComponent implements OnInit {
-@Input() parentData:string;
+@Input('asw') parentData:string;
 @Output() childEvent = new EventEmitter<string>();
 onChange(value:string){
 this.childEvent.emit(value);
